9eb919bb11e229b4d213328f8d4bd801f8517ff1
asianshyguy <hanck@usc.edu>
2018-03-03 14:37:04 -0800

    initial commit

---
+++ php/header.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+    <title>
+        <?php echo $title; ?>
+     </title>
+    <!-- <link rel="stylesheet" type="text/css" href="../../styles/main.css"> -->
+
+</head>
+
+<div id="nav">
+    <div class="temp_home">
+        <a href="../../index.html" class="home">
+            Home
+            </a>
+        </div>
+    <div id="nav_container">
+        <div id="nav_menu">
+            <a class="nav_link" href="../work.html">
+                Work
+                </a>
+            <a class="nav_link" href="../bio.html">
+                Bio
+                </a>
+            </div>
+        </div>
+    </div>
